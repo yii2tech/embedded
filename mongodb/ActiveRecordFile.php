bd46fcf1d0502187467943b0db2db9624c0d9d9b
Klimov Paul <klimov.paul@gmail.com>
2015-07-13 12:15:29 +0300

    ActiveRecord for 'mongodb' added

---
+++ mongodb/ActiveRecordFile.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link https://github.com/yii2tech
+ * @copyright Copyright (c) 2015 Yii2tech
+ * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
+ */
+
+namespace yii2tech\embedded\mongodb;
+
+use yii2tech\embedded\ContainerInterface;
+use yii2tech\embedded\ContainerTrait;
+
+/**
+ * ActiveRecord is an enhanced version of [[\yii\mongodb\file\ActiveRecord]], which includes 'embedded' functionality.
+ *
+ * Obviously, this class requires [yiisoft/yii2-mongodb](https://github.com/yiisoft/yii2-mongodb) extension installed.
+ *
+ * @see \yii\mongodb\file\ActiveRecord
+ *
+ * @author Paul Klimov <klimov.paul@gmail.com>
+ * @since 1.0
+ */
+class ActiveRecordFile extends \yii\mongodb\file\ActiveRecord implements ContainerInterface
+{
+    use ContainerTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeSave($insert)
+    {
+        if (!parent::beforeSave($insert)) {
+            return false;
+        }
+        $this->refreshFromEmbedded();
+        return true;
+    }
+}
